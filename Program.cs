07cb0d9dfeb2a56f79e12458c4d7a47ad44e810b
DenisKazmirchuk <161360416+DenisKazmirchuk@users.noreply.github.com>
2024-12-10 20:33:56 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] ratings = { 95, 88, 76, 59, 47, 100, 68, 85, 55, 91 };
+
+        // Запит 1: Кількість студентів із рейтингом < 60
+        int lowRatingCount = ratings.Count(r => r < 60);
+        Console.WriteLine($"Кількість студентів із рейтингом < 60: {lowRatingCount}");
+
+        // Запит 2: Кількість студентів із рейтингом > 90
+        int highRatingCount = ratings.Count(r => r > 90);
+        Console.WriteLine($"Кількість студентів із рейтингом > 90: {highRatingCount}");
+    }
+}
